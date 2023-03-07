04df81e0c0afb42a673cfde04a081fd0bd85c086
thddntjr1234 <thddntjr112233@gmail.com>
2023-03-08 03:22:36 +0900

    Chore: 기타 수정

--- src/main/java/com/ebstudy/board/v4/dto/response/PostListResponseDTO.java
+++ src/main/java/com/ebstudy/board/v4/dto/response/PostListResponseDTO.java
@@ -10,7 +10,6 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter
-@Setter
 @Builder
 public class PostListResponseDTO {
     List<CategoryDTO> categoryList;
--- src/main/java/com/ebstudy/board/v4/dto/response/PostResponseDTO.java
+++ src/main/java/com/ebstudy/board/v4/dto/response/PostResponseDTO.java
@@ -13,19 +13,7 @@ import java.util.List;
 @Setter
 @Builder
 public class PostResponseDTO {
-
     private PostDTO post;
-    // PostDTO vs 필요한 필드만 선언
-//    private Long postId;
-//    private Long hits;
-//    private Long categoryId;
-//    private String createdDate;
-//    private String modifiedDate;
-//    private String title;
-//    private String content;
-//    private String author;
-//    private String category;
-//    private String passwd;
     private List<FileDTO> fileList;
     private List<CommentDTO> commentList;
 
