c7b9414e30657e043de615b663afc0bf2e330e70
thddntjr1234 <thddntjr112233@gmail.com>
2023-03-24 03:22:43 +0900

    Feat: Bootstrap 적용

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "./router/router"
 import store from "./store/storage"
+import "bootstrap/dist/css/bootstrap.min.css" // [bootstrap]
+import "bootstrap" // [bootstrap]
+
 
 createApp(App).use(store).use(router).mount('#app')
