c862bb5dab30c1de371ed9aed7c6e972388e6b48
thddntjr1234 <thddntjr112233@gmail.com>
2023-04-16 17:30:33 +0900

    Feat: 로그인 컨트롤러 추가
    로그인 기능 정상동작 확인 완료

---
+++ src/main/java/com/ebstudy/board/v4/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.ebstudy.board.v4.controller;
+
+import com.ebstudy.board.v4.dto.LoginDTO;
+import com.ebstudy.board.v4.dto.TokenDTO;
+import com.ebstudy.board.v4.global.jwt.JwtFilter;
+import com.ebstudy.board.v4.global.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class AuthController {
+    
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+
+    /**
+     *
+     * @param loginDto
+     * @return
+     */
+    @PostMapping("/api/signin")
+    public ResponseEntity<TokenDTO> authorize(@Valid @RequestBody LoginDTO loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getLoginId(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtTokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDTO(jwt), httpHeaders, HttpStatus.OK);
+    }
+}
