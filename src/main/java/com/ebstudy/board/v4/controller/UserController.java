1acc6d77106091fd693ff97ba19a6c4c4d8bcefe
thddntjr1234 <thddntjr112233@gmail.com>
2023-04-29 02:32:00 +0900

    Refactor: AuthController 로그인 기능을 UserController로 이동
    - 로그인 컨트롤러는 유저 컨트롤러의 하위 기능이므로 별도로 AuthController로 두지 않고 통합

--- src/main/java/com/ebstudy/board/v4/controller/UserController.java
+++ src/main/java/com/ebstudy/board/v4/controller/UserController.java
@@ -1,23 +1,57 @@
 package com.ebstudy.board.v4.controller;
 
+import com.ebstudy.board.v4.dto.LoginDTO;
+import com.ebstudy.board.v4.dto.TokenDTO;
 import com.ebstudy.board.v4.dto.UserDTO;
+import com.ebstudy.board.v4.global.jwt.JwtFilter;
+import com.ebstudy.board.v4.global.jwt.JwtProvider;
 import com.ebstudy.board.v4.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Slf4j
-@RestController
 @RequiredArgsConstructor
+@RestController
 public class UserController {
 
+    private final JwtProvider jwtProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final UserService userService;
+
+    /**
+     * 유효한 로그인 요청에 대해 JWT 발급
+     * @param loginDto loginId, password
+     * @return JWT
+     */
+    @PostMapping("/api/signin")
+    public ResponseEntity<TokenDTO> signin(@Valid @RequestBody LoginDTO loginDto) {
+
+        // Controller에 비즈니스 로직이 들어갔기는 한데 지금은 단일 메소드만 가지고 있으니 일단 이대로 사용
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getLoginId(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDTO(jwt), httpHeaders, HttpStatus.OK);
+    }
 
     @GetMapping("/api/user")
     public ResponseEntity<Optional<UserDTO>> getMyUserInfo() {
