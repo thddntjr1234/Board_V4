99fa2589eeb95f52aa8d90b0ebc5be14e2a0dd11
thddntjr1234 <thddntjr112233@gmail.com>
2023-05-10 20:37:45 +0900

    Feat: SecurityConfig 인증 uri 추가
    - 프론트엔드에서 게시글 작성 폼 로딩에 인증과정 추가를 위한 uri 추가

--- src/main/java/com/ebstudy/board/v4/global/config/SecurityConfig.java
+++ src/main/java/com/ebstudy/board/v4/global/config/SecurityConfig.java
@@ -82,9 +82,10 @@ public class SecurityConfig {
                  //Comment
                 .antMatchers("/api/boards/comment").authenticated()
                  //Post
-                .antMatchers(HttpMethod.POST, "/api/board/free").authenticated()
-                .antMatchers(HttpMethod.PUT, "/api/board/free/**").authenticated()
-                .antMatchers(HttpMethod.DELETE, "/api/board/free/**").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/boards/free").authenticated()
+                .antMatchers(HttpMethod.PUT, "/api/boards/free/**").authenticated()
+                .antMatchers(HttpMethod.DELETE, "/api/boards/free/**").authenticated()
+                .antMatchers("/api/boards/free/new").access("isAuthenticated()")
                  //User
                 .antMatchers("/api/user/{userId}").access("isAuthenticated() and hasAnyRole('ADMIN')")
                 .antMatchers("/api/user").access("isAuthenticated() and hasAnyRole('USER', 'ADMIN')")
